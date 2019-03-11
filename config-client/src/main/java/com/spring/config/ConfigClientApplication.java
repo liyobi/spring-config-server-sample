fc0c4cf9f8a0153a6dc3bfef2f9d42821946e8fd
yongbin <liyobi@gmail.com>
2019-03-11 14:29:39 +0900

    config change

--- config-client/src/main/java/com/spring/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/spring/config/ConfigClientApplication.java
@@ -1,7 +1,11 @@
 package com.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
@@ -9,5 +13,19 @@ public class ConfigClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
-
+	
 }
+
+@RefreshScope
+@RestController
+class MessageRestController {
+ 
+    @Value("${msg:Hello world - Config Server is not working...error!!!!}")
+    private String msg;
+ 
+    @RequestMapping("/test")
+    public String test() {
+        return msg;
+    }
+}
+
